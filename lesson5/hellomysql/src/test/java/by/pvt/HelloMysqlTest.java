d1bc3ddb8e8d9741235a1645b5f0ca9e35f0aa7a
Bolotko Konstantsin <pro100kosti@gmail.com>
2019-07-08 21:07:24 +0300

    lesson5 (++)

--- lesson5/hellomysql/src/test/java/by/pvt/HelloMysqlTest.java
+++ lesson5/hellomysql/src/test/java/by/pvt/HelloMysqlTest.java
@@ -1,21 +1,50 @@
 package by.pvt;
 
 import static org.junit.Assert.*;
+
+import org.dbunit.DBTestCase;
+import org.dbunit.PropertiesBasedJdbcDatabaseTester;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.junit.Test;
 
+import java.io.FileInputStream;
 import java.sql.*;
 
-public class HelloMysqlTest {
+public class HelloMysqlTest extends DBTestCase {
+
+    public HelloMysqlTest(String name) {
+        super(name);
+        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "com.mysql.jdbc.Driver");
+        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, "jdbc:mysql://localhost:3306/hello_mysql_junit");
+        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, "root");
+        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, "gjhjkmghjcnjrjcnb");
+    }
+
+    @Override
+    protected IDataSet getDataSet() throws Exception {
+        return new FlatXmlDataSetBuilder().build(HelloMysqlTest.class.getResourceAsStream("system_users.xml"));
+    }
 
     @Test
-    public void testConnection(){
+    public void testConnection() {
 
         try {
             Connection connection =
-                    DriverManager.getConnection("jdbc:mysql://localhost:3306/hello_mysql", "root", "gjhjkmghjcnjrjcnb");
+                    DriverManager.getConnection("jdbc:mysql://localhost:3306/hello_mysql_junit", "root", "gjhjkmghjcnjrjcnb");
             PreparedStatement ps = connection.prepareStatement("select * from system_users");
 
             ResultSet rs = ps.executeQuery();
+
+            int rawCount = 0;
+            int activeUser = 0;
+
+            while (rs.next()) {
+                rawCount++;
+                if (rs.getBoolean("active")) activeUser++;
+            }
+            assertEquals(4, rawCount);
+            assertEquals(2, activeUser);
 
             assertNotNull(rs);
 
