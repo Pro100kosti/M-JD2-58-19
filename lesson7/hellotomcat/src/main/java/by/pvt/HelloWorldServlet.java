91374cb9c1da6bd2a7a11654088a46f83e95176c
Bolotko Konstantsin <pro100kosti@gmail.com>
2019-07-19 18:13:12 +0300

    lesson9+

--- lesson7/hellotomcat/src/main/java/by/pvt/HelloWorldServlet.java
+++ lesson7/hellotomcat/src/main/java/by/pvt/HelloWorldServlet.java
@@ -1,6 +1,10 @@
 package by.pvt;
 
+import by.pvt.dto.SystemUsers;
+import by.pvt.service.SystemUsersService;
+
 import java.io.IOException;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,10 +14,19 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(value="/helloWorld", name="helloWorldServlet")
 public class HelloWorldServlet extends HttpServlet {
 
+    private SystemUsersService systemUsersService;
+
+    @Override
+    public void init() throws ServletException {
+        systemUsersService = new SystemUsersService();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setStatus(200);
         resp.setHeader("Content-Type", "application/json");
+        List<SystemUsers> users = systemUsersService.getSystemUsers();
+        System.out.println("User size= " + users.size());
         resp.getWriter().print("{\"message\": \"hello, world!\"}");
     }
 
