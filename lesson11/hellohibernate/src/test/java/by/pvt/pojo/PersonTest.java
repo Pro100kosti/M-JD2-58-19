42c451cf6548271bf4a061fe362eec3451cbffed
Bolotko Konstantsin <pro100kosti@gmail.com>
2019-07-29 21:14:16 +0300

    lesson12

---
+++ lesson11/hellohibernate/src/test/java/by/pvt/pojo/PersonTest.java
@@ -0,0 +1,53 @@
+package by.pvt.pojo;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.junit.Test;
+import static by.pvt.util.HibernateUtil.*;
+
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class PersonTest {
+
+    @Test
+    public void testSavePerson() {
+        Session session = getInstance().getSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            Person p = createTestData();
+            session.save(p);
+            tx.commit();
+            session.close();
+
+            session = getInstance().getSession();
+            tx = session.beginTransaction();
+            List<Person> personList =
+                    session.createQuery("from person").list();
+            assertEquals(1, personList.size());
+            Person p2 = personList.get(0);
+            assertEquals(p, p2);
+            tx.commit();
+
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+    }
+
+    private static Person createTestData() {
+        Person person = new Person();
+        person.setId(1);
+        person.setFirstName("TestUser");
+        person.setLastName("LastName");
+        person.setDateOfBirth(new Date());
+        person.setGender('M');
+        return person;
+    }
+
+}
