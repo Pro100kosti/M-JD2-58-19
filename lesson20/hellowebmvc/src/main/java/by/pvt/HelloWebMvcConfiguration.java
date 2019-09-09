165b99988d9ebf743710cd6f438ca75202b8141a
Bolotko Konstantsin <pro100kosti@gmail.com>
2019-09-09 21:18:35 +0300

    lesson 20

---
+++ lesson20/hellowebmvc/src/main/java/by/pvt/HelloWebMvcConfiguration.java
@@ -0,0 +1,20 @@
+package by.pvt;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan("by.pvt")
+public class HelloWebMvcConfiguration {
+
+    @Bean
+    public InternalResourceViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver resolver =
+                new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
